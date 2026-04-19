ae487774f1296b9d87ba3ccfabc2f22d285aaf5e
SWAYAMJAISWAL <jaiswalswayam.jaiswal@gmail.com>
2026-04-19 13:03:08 +0530

    Food item component config
    Food item component config

---
+++ frontend/src/component/FoodItem/FoodItem.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import './foodItem.css'
+import { StoreContext } from '../../context/StoreContext'
+function FoodItem({id,name,description,price,image}) {
+    const {cartitems,addtocart,removefromcart} = React.useContext(StoreContext);
+  return (
+    
+    <>
+    <div className="food-item">
+        <div className="food-item-img-container">
+            <img className="food-item-img" src={image} alt="" />
+            {!cartitems[id] ? <img src={assets.add_icon_white} alt="" className="food-item-add-button" onClick={() => addtocart(id)} /> :
+            <div className="food-item-count-container">
+                <img src={assets.remove_icon_red} onClick={() => removefromcart(id)} alt="" />
+                    <p>{cartitems[id]}</p>
+                <img src={assets.add_icon_green} onClick={() => addtocart(id)} alt="" />
+            </div>
+
+            }
+        </div>
+        <div className="food-item-info">
+            <div className="food-item-name-rating">
+                <p>{name}</p>
+                <img src={assets.rating_starts} alt="" />
+            </div>
+            <p className="food-item-description">
+                {description}
+            </p>
+            <p className="food-item-price">
+                ${price}
+            </p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default FoodItem
